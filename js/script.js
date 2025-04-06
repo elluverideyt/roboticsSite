1472e7750b5f47ef9e5d288a7325121c059f3f58
elluveride <129328458+elluverideyt@users.noreply.github.com>
2025-04-06 04:50:43 +0000

    BIG COMMIT

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navToggle = document.querySelector('.nav-toggle');
-    const nav = document.querySelector('.main-nav ul');
-    navToggle.addEventListener('click', () => {
-        nav.classList.toggle('open');
-    });
-});
